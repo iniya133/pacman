836ccf3bfc3eafdb3c3455cfd351904475ccb8f7
Edouard Menayde <edouard.menayde@gmail.com>
2018-11-15 11:36:22 +0100

    First working level loading and level drawing

--- src/pacman/Game.java
+++ src/pacman/Game.java
@@ -1,5 +1,7 @@
 package pacman;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Observable;
 
 public class Game extends Observable {
@@ -7,9 +9,55 @@ public class Game extends Observable {
     private Entity[] entities;
     private int eateanPickables = 0;
     private int pickables;
+    private int size = 21;
+
+    private Slot characterToSlot(char character) throws Exception {
+        Slot slot;
+
+        switch (character) {
+            case 'X':
+                slot = new Wall();
+                break;
+            case 'O':
+                slot = new Corridor();
+                break;
+            default:
+                throw new Exception("Could not properly parse text file");
+        }
+
+        return slot;
+    }
 
     void load() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("assets/level-1.txt"))) {
+            String line = bufferedReader.readLine();
+            matrix = new Slot[size][size];
 
+            int y = 0;
+            while (line != null) {
+                char[] characters = line.toCharArray();
+                int x = 0;
+
+                for (char character : characters) {
+                    matrix[x][y] = characterToSlot(character);
+                    x += 1;
+                }
+
+                line = bufferedReader.readLine();
+                y += 1;
+            }
+            System.out.println(y);
+        } catch (Exception ex) {
+            System.err.println("Could not correctly process level file.");
+        }
+    }
+
+    Slot[][] getMatrix() {
+        return matrix;
+    }
+
+    int getSize() {
+        return size;
     }
 
     public boolean isEnded() {
--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -1,22 +1,53 @@
 package pacman;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane border = new BorderPane();
-        Scene root = new Scene(border, 600, 600);
+        int windowSize = 600;
+        Group root = new Group();
 
         Game game = new Game();
         game.load();
 
+        Canvas canvas = new Canvas(windowSize, windowSize);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+        int matrixSize = game.getSize();
+
+        int slotSize = windowSize / matrixSize;
+
+
+        Slot[][] matrix = game.getMatrix();
+
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                Slot slot = matrix[x][y];
+                System.out.println(slot);
+                if (slot instanceof Wall) {
+                    graphicsContext.setFill(Color.BLUE);
+                    System.out.println("Blue");
+                    graphicsContext.fillRect(x * slotSize, y * slotSize, slotSize, slotSize);
+                } else if (slot instanceof Corridor) {
+                    System.out.println("Black");
+                    graphicsContext.setFill(Color.BLACK);
+                    graphicsContext.fillRect(x * slotSize, y * slotSize, slotSize, slotSize);
+                }
+            }
+        }
+
+        root.getChildren().add(canvas);
+
         primaryStage.setTitle("Pacman");
-        primaryStage.setScene(root);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
