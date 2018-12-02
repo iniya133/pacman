1718c4f8b44ba2375c1d6f850d8dc36cb13b0bab
Edouard Menayde <edouard.menayde@gmail.com>
2018-12-02 17:50:07 +0100

    Make it impossible for threads to crash ui

--- src/pacman/entities/Ghost.java
+++ src/pacman/entities/Ghost.java
@@ -1,5 +1,6 @@
 package pacman.entities;
 
+import javafx.application.Platform;
 import pacman.Entity;
 import pacman.Game;
 import pacman.Direction;
@@ -26,110 +27,117 @@ public class Ghost extends Entity implements Runnable {
     public void run() {
         Random rand = new Random();
 
+        Entity entity = this;
+
         while (true) {
-            Slot[][] matrix = game.getMatrix();
-            int x = getPosition().x;
-            int y = getPosition().y;
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    Slot[][] matrix = game.getMatrix();
+                    int x = getPosition().x;
+                    int y = getPosition().y;
 
-            if (x == 0) {
-                x += 1;
-            } else if (x == game.getWidth() - 1) {
-                x = game.getWidth() - 2;
-            }
+                    if (x == 0) {
+                        x += 1;
+                    } else if (x == game.getWidth() - 1) {
+                        x = game.getWidth() - 2;
+                    }
 
-            boolean up = matrix[x][y - 1] instanceof Corridor || (!hasExited && matrix[x][y - 1] instanceof GhostDoor);
-            boolean down = matrix[x][y + 1] instanceof Corridor || (!hasExited && matrix[x][y + 1] instanceof GhostDoor);
-            boolean right = matrix[x + 1][y] instanceof Corridor || (!hasExited && matrix[x + 1][y] instanceof GhostDoor);
-            boolean left = matrix[x - 1][y] instanceof Corridor || (!hasExited && matrix[x - 1][y] instanceof GhostDoor);
+                    boolean up = matrix[x][y - 1] instanceof Corridor || (!hasExited && matrix[x][y - 1] instanceof GhostDoor);
+                    boolean down = matrix[x][y + 1] instanceof Corridor || (!hasExited && matrix[x][y + 1] instanceof GhostDoor);
+                    boolean right = matrix[x + 1][y] instanceof Corridor || (!hasExited && matrix[x + 1][y] instanceof GhostDoor);
+                    boolean left = matrix[x - 1][y] instanceof Corridor || (!hasExited && matrix[x - 1][y] instanceof GhostDoor);
 
-            if (matrix[x][y] instanceof GhostDoor) {
-                currentDirection = Direction.UP;
-                hasExited = true;
-            } else {
-                int two_choice = rand.nextInt(2);
-                int three_choice = rand.nextInt(3);
+                    if (matrix[x][y] instanceof GhostDoor) {
+                        currentDirection = Direction.UP;
+                        hasExited = true;
+                    } else {
+                        int two_choice = rand.nextInt(2);
+                        int three_choice = rand.nextInt(3);
 
-                switch (currentDirection) {
-                    case UP:
-                        if (up && right && left) {
+                        switch (currentDirection) {
+                            case UP:
+                                if (up && right && left) {
 
-                        } else if (right && left) {
-                            currentDirection = Direction.RIGHT;
-                        } else if (right && up) {
-                            currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.UP;
-                        } else if (left && up) {
-                            currentDirection = two_choice == 0 ? Direction.LEFT : Direction.UP;
-                        } else if (up) {
-                            currentDirection = Direction.UP;
-                        } else if (left) {
-                            currentDirection = Direction.LEFT;
-                        } else if (right) {
-                            currentDirection = Direction.RIGHT;
+                                } else if (right && left) {
+                                    currentDirection = Direction.RIGHT;
+                                } else if (right && up) {
+                                    currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.UP;
+                                } else if (left && up) {
+                                    currentDirection = two_choice == 0 ? Direction.LEFT : Direction.UP;
+                                } else if (up) {
+                                    currentDirection = Direction.UP;
+                                } else if (left) {
+                                    currentDirection = Direction.LEFT;
+                                } else if (right) {
+                                    currentDirection = Direction.RIGHT;
+                                }
+                                break;
+
+                            case RIGHT:
+                                if (up && right && down) {
+                                    currentDirection = Direction.UP;
+                                } else if (up && down) {
+                                    currentDirection = Direction.UP;
+                                } else if (right && up) {
+                                    currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.UP;
+                                } else if (down && right) {
+                                    currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
+                                } else if (right) {
+                                    currentDirection = Direction.RIGHT;
+                                } else if (down) {
+                                    currentDirection = Direction.DOWN;
+                                } else if (up) {
+                                    currentDirection = Direction.UP;
+                                }
+                                break;
+
+
+                            case LEFT:
+                                if (up && left && down) {
+                                    currentDirection = three_choice == 0 ? Direction.UP : (three_choice == 1 ? Direction.LEFT : Direction.DOWN);
+                                } else if (up && down) {
+                                    currentDirection = two_choice == 0 ? Direction.UP : Direction.DOWN;
+                                } else if (left && up) {
+                                    currentDirection = two_choice == 0 ? Direction.LEFT : Direction.UP;
+                                } else if (down && left) {
+                                    currentDirection = two_choice == 0 ? Direction.LEFT : Direction.DOWN;
+                                } else if (left) {
+                                    currentDirection = Direction.LEFT;
+                                } else if (down) {
+                                    currentDirection = Direction.DOWN;
+                                } else if (up) {
+                                    currentDirection = Direction.UP;
+                                }
+                                break;
+
+                            case DOWN:
+                                if (down && right && left) {
+                                    currentDirection = three_choice == 0 ? Direction.DOWN : (three_choice == 1 ? Direction.LEFT : Direction.LEFT);
+                                } else if (right && left) {
+                                    currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
+                                } else if (right && down) {
+                                    currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
+                                } else if (left && down) {
+                                    currentDirection = two_choice == 0 ? Direction.LEFT : Direction.DOWN;
+                                } else if (down) {
+                                    currentDirection = Direction.DOWN;
+                                } else if (left) {
+                                    currentDirection = Direction.LEFT;
+                                } else if (right) {
+                                    currentDirection = Direction.RIGHT;
+                                }
+                                break;
+
                         }
-                        break;
+                    }
 
-                    case RIGHT:
-                        if (up && right && down) {
-                            currentDirection = Direction.UP;
-                        } else if (up && down) {
-                            currentDirection = Direction.UP;
-                        } else if (right && up) {
-                            currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.UP;
-                        } else if (down && right) {
-                            currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
-                        } else if (right) {
-                            currentDirection = Direction.RIGHT;
-                        } else if (down) {
-                            currentDirection = Direction.DOWN;
-                        } else if (up) {
-                            currentDirection = Direction.UP;
-                        }
-                        break;
-
-
-                    case LEFT:
-                        if (up && left && down) {
-                            currentDirection = three_choice == 0 ? Direction.UP : (three_choice == 1 ? Direction.LEFT : Direction.DOWN);
-                        } else if (up && down) {
-                            currentDirection = two_choice == 0 ? Direction.UP : Direction.DOWN;
-                        } else if (left && up) {
-                            currentDirection = two_choice == 0 ? Direction.LEFT : Direction.UP;
-                        } else if (down && left) {
-                            currentDirection = two_choice == 0 ? Direction.LEFT : Direction.DOWN;
-                        } else if (left) {
-                            currentDirection = Direction.LEFT;
-                        } else if (down) {
-                            currentDirection = Direction.DOWN;
-                        } else if (up) {
-                            currentDirection = Direction.UP;
-                        }
-                        break;
-
-                    case DOWN:
-                        if (down && right && left) {
-                            currentDirection = three_choice == 0 ? Direction.DOWN : (three_choice == 1 ? Direction.LEFT : Direction.LEFT);
-                        } else if (right && left) {
-                            currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
-                        } else if (right && down) {
-                            currentDirection = two_choice == 0 ? Direction.RIGHT : Direction.DOWN;
-                        } else if (left && down) {
-                            currentDirection = two_choice == 0 ? Direction.LEFT : Direction.DOWN;
-                        } else if (down) {
-                            currentDirection = Direction.DOWN;
-                        } else if (left) {
-                            currentDirection = Direction.LEFT;
-                        } else if (right) {
-                            currentDirection = Direction.RIGHT;
-                        }
-                        break;
-
+                    game.move(entity, currentDirection);
                 }
-            }
-
-            game.move(this, currentDirection);
+            });
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
